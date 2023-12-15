dbf18a467105820473e2a9d9f00acab7d03b903a
biancaaesteves <biancaaesteves@icloud.com>
2023-12-15 09:53:31 -0300

    projeto conta-banco

--- desafios/conta-banco/src/ContaTerminal.java
+++ desafios/conta-banco/src/ContaTerminal.java
@@ -1,3 +1,23 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
-    
+    public static void main(String[] args) throws Exception {
+
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite seu nome: ");
+        String nome = sc.next();
+        System.out.println("Por favor, digite o número da agência:");
+        int agencia = sc.nextInt();
+        System.out.println("Usuário: ");
+        int conta = sc.nextInt();
+        System.out.println("Valor do depósito: ");
+        double saldo = sc.nextDouble();
+
+        System.out.printf("Olá, %s, obrigado por criar uma conta em nosso banco! Sua agência é %d, sua conta é %d e seu saldo é %.2f.%n", nome, agencia, conta, saldo);
+
+sc.close();
+      
+    }
 }
